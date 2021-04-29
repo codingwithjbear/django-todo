a0a17a4c4f4c4a8256e0ec08017cb6d3bbb6a130
codingwithjbear <hatg92@gmail.com>
2021-04-29 12:43:43 -0500

    Todo model/dbtable created

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True) #optional 
+    important = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True) # specific date and time. anytime a new "todo" object is created it will be givien the value automaticaly  
+    completed = models.DateTimeField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+   
+    def __str__(self):
+        return self.title
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def signupuser(request):
@@ -30,8 +30,23 @@ def logoutuser(request):
     if request.method == 'POST': # has to be post request so that the borwser doesn't log you out 
         logout(request)
         return redirect('home')
+
 def loginuser(request):
-    pass
+    htmlpath = "todo/loginuser.html"
+    if request.method == 'GET':
+        return render(request, htmlpath, {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])        
+        if user is None:
+            return render(request, htmlpath, {'form':AuthenticationForm(), 'error':'Username and password did not match'})
+        else:
+            #(you were able to authinticate them, if the user was found and password matched then log them in
+            login(request, user)
+            return redirect('currenttodos')
+
+
+
+
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')
 
